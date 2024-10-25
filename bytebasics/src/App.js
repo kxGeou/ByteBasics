bf16f638d73dfc45cf5bd0dcb6057c0d495ddb6c
kxGeou <grzegorzm.work@gmail.com>
2024-10-25 07:50:31 +0200

    mini changes

--- bytebasics/src/App.js
+++ bytebasics/src/App.js
@@ -3,9 +3,11 @@ import Header from './components/header';
 import HeroSection from './components/heroSection';
 function App() {
   return (
-    <div className="MainPage">
-       <Header></Header>
-       <HeroSection></HeroSection>
+    <div className="wrapper">
+      <div className="MainPage">
+        <Header></Header>
+        {/* <HeroSection></HeroSection> */}
+      </div>
     </div>
   );
 }
--- bytebasics/src/components/header.js
+++ bytebasics/src/components/header.js
@@ -2,20 +2,16 @@ import '../style/mainPage.scss';
 import InstagramLogo from '../resources/Icons/instagramVector.jpg';
 import GitHubIcon from '../resources/Icons/gitVector.jpg';
 import FacebookIcon from '../resources/Icons/facebookVector.jpg';
-import UserIcon from '../resources/Icons/userIcon.png';
 function Header() {
   return (
     <div className="MainPage">
         <header>
             <div className="iconsBox">
-              <img src={InstagramLogo} alt="instagram logo"></img>
-              <img src={GitHubIcon} alt="github logo"></img>
+              <img src={InstagramLogo} alt="instagram logo" className="h-10"></img>
+              <img src={GitHubIcon} alt="github logo" className="h-10"></img>
               <img src={FacebookIcon} alt="facebook logo"></img>
             </div>
             <div className="companyLogo"></div>
-            <div className="userIcon">
-              <img src={UserIcon} alt="user icon"></img>
-            </div>
         </header> 
         
     </div>
