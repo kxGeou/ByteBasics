e9156ae6f533be3b2974ec978c63888f1c55945e
kxGeou <grzegorzm.work@gmail.com>
2024-11-01 18:23:53 +0100

    qwe

--- bytebasics/src/pages/HTML/MainHTML.jsx
+++ bytebasics/src/pages/HTML/MainHTML.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import Header from '../../components/mainPage/header'
-import Footer from '../../assets/footer'
+import { useState } from 'react';
+// import Header from '../../components/mainPage/header'
+// import Footer from '../../assets/footer'
+import LessonData from '../../HTMLData.json';
 function MainHTML() {
+  const [lessons, setLesson] = useState(LessonData.slice(0,3));
+
   return (
     <div>
-      <Header></Header>
-      <Footer></Footer>
+      {lessons.map((lesson) => {
+        return(
+          <div className='lesson'>
+            <h3>{lesson.id}</h3>
+            <h3>{lesson.Name}</h3>
+            <h3>{lesson.Description}</h3>
+          </div>
+        )
+      })}
+
     </div>
   )
 }   
