8337417b1b8af82ada9692c1394d8b1a2ec53183
kxGeou <grzegorzm.work@gmail.com>
2024-11-22 08:46:04 +0100

    Update header.jsx

--- bytebasics/src/components/Header/header.jsx
+++ bytebasics/src/components/Header/header.jsx
@@ -3,15 +3,6 @@ import { useCallback } from 'react';
 import data from '../../DataBase/mainData/data.json';
 import Logo from '../../resources/logo.png';
 function Header() {
-  const links = {
-    instagramLink: "https://www.instagram.com/kxgeou/",
-    gitHubLink: "https://github.com/kxGeou",
-    facebookLink: "https://www.facebook.com/profile.php?id=100073536213869"
-  };
-  
-  const handleLinkClick = useCallback((url) => {
-      window.open(url, "_blank");
-  }, []);
   return (
     <div className='wrapper'>
       <header>
